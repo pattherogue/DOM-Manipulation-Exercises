f0c1ccc87a4b286bd05b73f674f6fb1a757bb45e
Paolo Gomez <peweegardner248@gmail.com>
2024-01-22 23:24:34 -0800

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,35 @@ document.querySelectorAll(".second")
 document.querySelector("ol .third");
 
 //Give the section with an id of container the text “Hello!”.
+let foundDiv = document.querySelector("#container");
+
+foundDiv.innerText = "Hello";
+
+//Add the class main to the div with a class of footer.
+let footer = document.querySelector(".footer");
+footer.classList.add("main");
+
+//Remove the class main on the div with a class of footer.
+let footer = document.querySelector(".footer");
+footer.classList.remove("main");
+
+//Create a new li element.
+let newLi = document.createElement("li");
+
+//Give the li the text “four”.
+newLi.innerText = "four"
+
+//Append the li to the ul element.
+let list = document.querySelector("ul");
+list.appendChild(newLi);
+
+//Loop over all of the lis inside the ol tag and give them a background color of “green”.
+let liInsideOl = document.querySelectorAll("ol li");
+
+for(let i = 0; i < liInsideOl.length; i++){
+    liInsideOl[i].style.backgroundColor = "green";
+}
+
+//Remove the div with a class of footer.
+let footer = document.querySelector(".footer");
+footer.remove();
