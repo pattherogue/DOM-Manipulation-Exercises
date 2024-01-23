c85d9d359e23d4f49c8beca2a67faf78c2584913
Paolo Gomez <peweegardner248@gmail.com>
2024-01-22 23:11:18 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//Select the section with an id of container without using querySelector.
+document.getElementById("container");
+
+//Select the section with an id of container using querySelector.
+document.querySelector("#container")
+
+//Select all of the list items with a class of “second”.
+document.querySelectorAll(".second")
+
+//Select a list item with a class of third, but only the list item inside of the ol tag.
+document.querySelector("ol .third");
+
+//Give the section with an id of container the text “Hello!”.
